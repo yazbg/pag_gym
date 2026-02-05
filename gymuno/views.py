cfc8e6a254830660a137d24eb3dce1f363495501
Yazmin Bacilio Santiago <yaaz.bg@gmail.com>
2026-02-05 16:13:33 -0600

    Chore: dejar de rastrear la base de datos local

--- gymuno/views.py
+++ gymuno/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_view(request):
-
-    
-    return render(request, 'machine_list.html') #las vistas siempre retornan un render
+    machines = [
+        {'title': 'Treadmill'},
+        {'title': 'Exercise Bike'},
+        {'title': 'Rowing Machine'},
+    ]
+    contex = {
+        'machines': machines       
+    }
+    return render(request, 'machine_list.html', contex) #las vistas siempre retornan un render
